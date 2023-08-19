4af0aeb0034474350fab3000cc1d382707f11852
BeccaNaude <beccanaude@gmail.com>
2023-08-19 16:30:37 +0200

    added & modified new file helloWorld.py

---
+++ helloWorld.py
@@ -0,0 +1,9 @@
+# LEVEL 3, TASK 04 - VERSION CONTROL II: GIT BASICS
+# COMPULSORY TASK 01
+
+'''write a program that prints out the message "Hello World!"'''
+'''change the message printed out by the program to "Git is awesome!"'''
+# message
+message = "Git is awesome!"
+# print message
+print(message)
