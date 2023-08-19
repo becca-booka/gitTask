eb20aa1ea671cda9f2a2210daef058019315b50e
BeccaNaude <beccanaude@gmail.com>
2023-08-19 16:40:51 +0200

    modified helloWorld.py file

--- helloWorld.py
+++ helloWorld.py
@@ -3,7 +3,8 @@
 
 '''write a program that prints out the message "Hello World!"'''
 '''change the message printed out by the program to "Git is awesome!"'''
+'''modify your program to accept input from the user and then print out the inputted data'''
 # message
-message = "Git is awesome!"
+message = input("Enter your message her: \n")
 # print message
 print(message)
