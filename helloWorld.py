0a859a3a7587a8a66224c6c557439b4368a4edf8
BeccaNaude <beccanaude@gmail.com>
2023-09-05 17:05:47 +0200

    changed program to ask for and print out user's name

--- helloWorld.py
+++ helloWorld.py
@@ -4,7 +4,11 @@
 '''write a program that prints out the message "Hello World!"'''
 '''change the message printed out by the program to "Git is awesome!"'''
 '''modify your program to accept input from the user and then print out the inputted data'''
+'''modify your program to ask the user their name and then print it out'''
+# ask for user's name
+user_name = input("Please enter your name: \n")
 # message
-message = input("Enter your message her: \n")
-# print message
+message = input("Enter your message here: \n")
+# print name and message
+print(user_name)
 print(message)
